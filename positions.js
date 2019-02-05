487833c28b625d8ed7cba668b849ae99da6c9997
Christian Lara <clara@intersysconsulting.com>
2019-02-05 15:37:43 -0600

    christian_lara

--- positions.js
+++ positions.js
@@ -1,5 +1,7 @@
 
 const data = [
-  { "lt":25.5315788, "lg":-103.4031409 } //Christian Medinilla
-  { "lt":25.5438839, "lg":-103.5276588 } //Jorge Molina
+
+  { "lt":25.5315788, "lg":-103.4031409 }, //Christian Medinilla
+  { "lt":25.5438839, "lg":-103.5276588 }, //Jorge Molina
+  { "lt":21.1280261, "lg":-101.6904704 } //Christian Lara
 ];
