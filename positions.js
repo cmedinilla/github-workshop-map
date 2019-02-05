98054142e5fd1666a11ccc93a58d65a78c5dd9e6
Hector Mares <hmares@intersysconsulting.com>
2019-02-05 15:39:09 -0600

    first

--- positions.js
+++ positions.js
@@ -2,5 +2,7 @@ const data = [
     { "lt": 25.5315788, "lg": -103.4031409 }, //Christian Medinilla
     { "lt": 25.5438839, "lg": -103.5276588 }, //Jorge Molina
     { "lt": 21.1280261, "lg": -101.6904704 }, //Christian Lara
-    { "lt": 20.6736, "lg": -103.344 } //Triunfo Bobadilla
-];
+    { "lt": 20.6736, "lg": -103.344 }, //Triunfo Bobadilla
+    { "lt":20.6468764, "lg":-103.3096554 } //Hector Mares
+  ];
+  
