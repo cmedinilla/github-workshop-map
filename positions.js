4362fe5d94cd591a4cb031db0bd96c3ef5f2b0fa
Triunfo <jbobadilla@intersysconsulting.com>
2019-02-05 15:40:32 -0600

    Triunfo Bobadilla

--- positions.js
+++ positions.js
@@ -1,6 +1,6 @@
-
 const data = [
-  { "lt":25.5315788, "lg":-103.4031409 }, //Christian Medinilla
-  { "lt":25.5438839, "lg":-103.5276588 }, //Jorge Molina
-  { "lt":21.1280261, "lg":-101.6904704 } //Christian Lara
+    { "lt": 25.5315788, "lg": -103.4031409 }, //Christian Medinilla
+    { "lt": 25.5438839, "lg": -103.5276588 }, //Jorge Molina
+    { "lt": 21.1280261, "lg": -101.6904704 }, //Christian Lara
+    { "lt": 20.6736, "lg": -103.344 } //Triunfo Bobadilla
 ];
