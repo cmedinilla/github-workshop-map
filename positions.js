ef7c6e9eb39f68dbfa9d358f8aadfb8bb2054c61
Lucero Nava <lnava@intersysconsulting.com>
2019-02-05 15:42:58 -0600

    Firt commit

--- positions.js
+++ positions.js
@@ -3,5 +3,6 @@ const data = [
     { "lt": 25.5438839, "lg": -103.5276588 }, //Jorge Molina
     { "lt": 21.1280261, "lg": -101.6904704 }, //Christian Lara
     { "lt": 20.6736, "lg": -103.344 }, //Triunfo Bobadilla
-    { "lt":20.6468764, "lg":-103.3096554 } //Hector Mares
+    { "lt":20.6468764, "lg":-103.3096554 }, //Hector Mares
+    {"lt":20.6336119,"lg":-103.4659556}//Lucero Nava
   ];
