792ea1f85d6cccdeb10cf41596e2b5d853a45b2b
MkHunter <miguelernesto_23@hotmail.com>
2021-11-16 16:28:28 -0600

    Modify positions data

--- positions.js
+++ positions.js
@@ -1,4 +1,8 @@
 const data = [
+<<<<<<< HEAD
     {"lt": 25.5315788, "lg": -103.4031409, "name": "Christian Medinilla"},
     {"lt": 22.233105, "lg": -97.861099, "name": "Francisco Ventura"},
+=======
+    {"lt": 52.4803739, "lg": 62.1834362, "name": "Mike"},
+>>>>>>> 06a9715 (Modify positions data)
 ];
