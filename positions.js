e277cea5b3cc2821c93ff7754e56a1fe22b651a1
Roberto Lazo <roberto.lazo@alumnos.udg.mx>
2021-11-16 16:34:50 -0600

    ADD: add a new positions

--- positions.js
+++ positions.js
@@ -1,7 +1,11 @@
 const data = [
     {"lt": 25.5315788, "lg": -103.4031409, "name": "Christian Medinilla"},
+<<<<<<< HEAD
     {"lt": 22.233105, "lg": -97.861099, "name": "Francisco Ventura"},
     {"lt": 52.4803739, "lg": 62.1834362, "name": "Mike"},
     {"lt":20.593684, "lg":78.96288, "name":"Antonio Rodriguez Barrientos"},
     {"lt": 20.6736, "lg": -103.344, "name": "Roberto Lazo"},
+=======
+    {"lt": 20.6736, "lg": -103.344, "name": "Roberto Lazo"}
+>>>>>>> 1796ff0 (ADD: add a new positions)
 ];
