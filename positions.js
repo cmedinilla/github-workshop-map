5d588f8e0dad7012a9276898535b013fd1cf7954
Francisco Ventura <franj.vents@gmail.com>
2021-11-16 16:36:05 -0600

    Add trailing comma to positions.js

--- positions.js
+++ positions.js
@@ -1,4 +1,4 @@
 const data = [
     {"lt": 25.5315788, "lg": -103.4031409, "name": "Christian Medinilla"},
-    {"lt": 22.233105, "lg": -97.861099, "name": "Francisco Ventura"}
+    {"lt": 22.233105, "lg": -97.861099, "name": "Francisco Ventura"},
 ];
