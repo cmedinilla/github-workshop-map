a72e9cc28e6105d83b5d270d56c14f52313f3479
PacoV3 <franj.vents@gmail.com>
2021-11-16 17:55:25 -0600

    Change longitudes to negative

--- positions.js
+++ positions.js
@@ -1,8 +1,8 @@
 const data = [
     {"lt": 25.5315788, "lg": -103.4031409, "name": "Christian Medinilla"},
     {"lt": 22.233105, "lg": -97.861099, "name": "Francisco Ventura"},
-    {"lt": 52.4803739, "lg": 62.1834362, "name": "Mike"},
-    {"lt":20.593684, "lg":78.96288, "name":"Antonio Rodriguez Barrientos"},
+    {"lt": 52.4803739, "lg": -62.1834362, "name": "Mike"},
+    {"lt": 20.593684, "lg": -78.96288, "name": "Antonio Rodriguez Barrientos"},
     {"lt": 20.6736, "lg": -103.344, "name": "Roberto Lazo"},
-    {"lt":19.4785100, "lg":-99.2396300, "name":"Manuel Sanchez"},
+    {"lt": 19.4785100, "lg": -99.2396300, "name": "Manuel Sanchez"},
 ];
