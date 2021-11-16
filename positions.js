db68d363213efe76e259f055135f71a611cd35bf
antonio1998abri <ANTONIO.RBARRIENTOS@ALUMNOS.UDG.MX>
2021-11-16 17:07:20 -0600

    Add position

--- positions.js
+++ positions.js
@@ -1,5 +1,10 @@
 const data = [
+<<<<<<< HEAD
     {"lt": 25.5315788, "lg": -103.4031409, "name": "Christian Medinilla"},
     {"lt": 22.233105, "lg": -97.861099, "name": "Francisco Ventura"},
     {"lt": 52.4803739, "lg": 62.1834362, "name": "Mike"},
+=======
+  {"lt":20.593684, "lg":78.96288, "name":"Antonio Rodriguez Barrientos"},
+
+>>>>>>> 81b13c1 (Update Content of Data)
 ];
