bc8831abd253c3a43d12fdbc5de0070be3857375
Otreb0r <42620567+Otreb0r@users.noreply.github.com>
2021-11-16 17:29:34 -0600

    Change: New element (#48)
    * Change: New elements
    
    * Change: New element
    
    * ADD: add a new positions
    
    * UPDATE: update positions.js file
    
    * CHANGE: positions.js

--- positions.js
+++ positions.js
@@ -3,4 +3,5 @@ const data = [
     {"lt": 22.233105, "lg": -97.861099, "name": "Francisco Ventura"},
     {"lt": 52.4803739, "lg": 62.1834362, "name": "Mike"},
     {"lt":20.593684, "lg":78.96288, "name":"Antonio Rodriguez Barrientos"},
+    {"lt": 20.6736, "lg": -103.344, "name": "Roberto Lazo"},
 ];
